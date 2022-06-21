2c84133229835675bdcf4b835e5c573510b0d042
Ronak Jain <ronak29jain@gmail.com>
2022-06-21 13:05:07 +0530

    basic adding item in list

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var inputitem = document.getElementById("inputitem")
-var button = document.getElementById("button")
-var ul = document.getElementsByTagName("ul")
+var inputitem = document.getElementById("inputitem");
+var button = document.getElementById("button");
+var ul = document.querySelector("ul");
 
+function inputlength() {
+    return inputitem.value.length;
+}
+
+function additem (){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(inputitem.value));
+    ul.appendChild(li)
+    inputitem.value = '';
+}
+
+button.addEventListener('click', function(){
+    if (inputlength() > 0){
+        additem ();
+    }
+})
+
+inputitem.addEventListener('keypress', function(event){
+    if (inputlength() > 0 && event.code === 'Enter'){
+        additem();
+    }
+})
+''
